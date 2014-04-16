5c6eeb27b882227fb2b214eba231c0ec6a0ae1d0
Andrius Skerla <andrius@skerla.com>
2014-04-16 16:37:06 +0100

    moved config to the ghome dir

--- proxy.js
+++ proxy.js
@@ -4,54 +4,54 @@ var hosts = require('./lib/hosts');
 var commander = require('commander');
 
 commander
-    .version('0.0.1')
-    .option('-p, --port <n>', 'Port to listen to', 80)
-    .parse(process.argv);
+  .version('0.0.1')
+  .option('-p, --port <n>', 'Port to listen to', 80)
+  .parse(process.argv);
 
 var proxy = httpProxy.createProxyServer({
-    ws: true,
-    xfwd: true
+  ws: true,
+  xfwd: true
 });
-
 
 
 proxy.on('error', function (err, req, res) {
-    res.writeHead(500, {
-        'Content-Type': 'text/plain'
-    });
+  var target = hosts(req.headers.host.split(':')[0]);
 
-    console.log('Proxy error', err, req.headers);
-    res.end('Something went wrong. No worries it\'s reported.');
+  res.writeHead(500, {
+    'Content-Type': 'text/plain'
+  });
+
+  console.log('Proxy error', err, target);
+  res.end('That\'s a pretty fookin bad gateway!');
 });
 
-
 var server = http.createServer(function (req, res) {
-    var target = hosts(req.headers.host.split(':')[0]);
+  var target = hosts(req.headers.host.split(':')[0]);
 
-    if (target) {
-        proxy.web(req, res, {
-            target: target
-        });
-    } else {
-        res.statusCode = 500;
-        res.end('Oooopsy... The host was not found.');
-    }
+  if (target) {
+    proxy.web(req, res, {
+      target: target
+    });
+  } else {
+    res.statusCode = 500;
+    res.end('Oooopsy... The host was not found.');
+  }
 
 });
 
 server.on('upgrade', function (req, socket, head) {
-    var target = hosts(req.headers.host.split(':')[0]);
+  var target = hosts(req.headers.host.split(':')[0]);
 
-    if (target) {
-        proxy.ws(req, socket, head, {
-            target: target
-        });
-    } else {
-        socket.write('Oooopsy... The host was not found.');
-        socket.end()
-    }
+  if (target) {
+    proxy.ws(req, socket, head, {
+      target: target
+    });
+  } else {
+    socket.write('Oooopsy... The host was not found.');
+    socket.end()
+  }
 });
 
 server.listen(commander.port, function () {
-    console.log('Proxy server is listening on ' + commander.port);
+  console.log('Proxy server is listening on ' + commander.port);
 });
